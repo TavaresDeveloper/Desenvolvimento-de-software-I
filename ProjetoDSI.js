80dfd7e31ea8fbc68be91bd568d05229cb18ae09
TavaresDeveloper <Tavares_23456@outlook.com>
2025-03-11 13:35:25 -0300

    validação de login

--- ProjetoDSI.js
+++ ProjetoDSI.js
@@ -1,2 +1,34 @@
-var email = getElementById('e-mail');
-var passoword = getElementById('senha');
+var email = document.getElementById('e-mail');
+var passoword = document.getElementById('senha');
+
+
+function login(event){
+
+   event.preventDefault();
+
+     var valorEmail = email.value;
+     var valorSenha = passoword.value;
+
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if(!emailRegex.test(valorEmail)){
+
+        alert('email invalido! Por favor, tente novamente.');
+
+        return false;
+
+    }
+
+    if(valorSenha.length < 6){
+
+        alert("Senha muita fraca! Por gentileza, fortifique-a.");
+        return false;
+
+    }
+
+    alert("Login efetuado com sucesso!");
+
+}
+
+var formulario = document.GetElementById('LoginForm');
+formulario.onsubmit = login;
